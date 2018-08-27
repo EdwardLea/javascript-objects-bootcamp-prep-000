9c3f249d8e6e0fd208dcdc23e66dbdccdeadd460
leaedward87@gmail.com <leaedward87@gmail.com>
2018-08-27 19:24:06 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,5 +1,6 @@
 var playlist = {aristname1: "song title1", aristname2: "song title2"};
 
 function updatePlaylist (playlist, aristname, songname){
-  
+  playlist[aristname] = songname;
+  return playlist;
 }
