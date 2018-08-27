7a2cbaecdeee0039ccfb6b4c64f0c8a6c547c505
leaedward87@gmail.com <leaedward87@gmail.com>
2018-08-27 19:40:25 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -6,6 +6,6 @@ function updatePlaylist (playlist, aristname, songname){
 }
 
 function removeFromPlaylist (playlist, aristName){
-  delete playlist['aristName'];
+  delete playlist.aristName;
   return playlist;
 }
